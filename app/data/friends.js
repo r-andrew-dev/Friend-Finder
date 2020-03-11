84bf371d48db77f3de483eb172e89195e7088c03
Rachael Andrew <r.andrew.dev@gmail.com>
2020-03-10 22:42:26 -0500

    built out survey object, designed first question asthetic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,10 @@
-let count = 0
-// const survey = {
-//     questions: ['']
-    
+// let count = 0
+// // const survey = {
+// //     questions: ['']
 
-$("#survey-button").on('click', function() {
-    console.log("clicked")
-    $("#start-survey").fadeOut("slow", function() {
-        $("#start-question").fadeIn("slow");
-});
-});
+// $("#survey-button").on('click', function() {
+//     console.log("clicked-again")
+//     $("#start-survey").fadeOut("slow", function() {
+//         $("#start-question").fadeIn("slow");
+// });
+// });
--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,6 +1,23 @@
-$("#survey-button").on('click', function() {
-    console.log("clicked")
-    $("#start-survey").fadeOut("slow", function() {
-        $("#questions").fadeIn("slow");
+let count = 0
+const survey = {
+    questions: ['What is your favorite season?', name + ", how much wood could a woodchuck chuck if a woodchuck could chuck wood?", 
+                 "How much does proper lighting design turn you on?", "Political Views?", "Oh hey, " + name + " let's talk sweet, sweet OS(s)",
+                "Start the morning drink of choice?", "How likely are you to win speaker of the year?", name + " now do you think you'll find your match?"],
+    ranges:{rangeLeft:["winter", "woodchucks don't do that", "That's weird", "European Left", "Linux", "water", "wordsHard", "gonna try Tinder"], rangeRight:["summer", "8.8 tons everyday", "It makes me spark", "Alt-Right", "Mac", "Coffee=BlackerThanSoul", "brevity=SoulOfWit", "1st date planned"]},
+    imageUrls: ["4-seasons.png", "5-woodchuck.jpg", "6-lighting.png", "7-politics.png", "8-sweetOs.jpg", "9-drinkofchoice.png", "10-speaker.jpg", "11-brain.png"],
+}
+
+
+$("#survey-button").on('click', function () {
+    console.log("clicked-again")
+    $("#start-survey").fadeOut("slow", function () {
+        $("#start-question").fadeIn("slow");
+    });
 });
+
+$("#question-startButton").on('click', function () {
+    console.log("CLICKED THAT")
+    $("#start-question").fadeOut("slow", function () {
+        $("#questions").fadeIn("slow");
+    });
 });
